d8d371c87030379045c36a1bb786cc503d58e4a0
DanskCarvalho <danskcarvalho@gmail.com>
2021-10-01 18:26:04 -0300

    Continuar amanhã..

--- Sources/Pulsar.CommandHandlers/Acompanhamentos/AcompanhamentoCommandHandler.cs
+++ Sources/Pulsar.CommandHandlers/Acompanhamentos/AcompanhamentoCommandHandler.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Pulsar.Common.Cqrs;
+using Pulsar.Contracts.Acompanhamentos.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pulsar.CommandHandlers
 {
-    public class AcompanhamentoCommandHandler
+    public class AcompanhamentoCommandHandler :
+        IAsyncCommandHandler<CancelarAcompanhamentoCommand>
     {
+        public Task Handle(ICommand cmd, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
